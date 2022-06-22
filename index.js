e5a0826de9616b9248f87c094eeaff5cd99cf7ae
Mauricio Simón Rodriguez <maurizio_1979_r@hotmail.com>
2022-06-22 16:25:43 -0300

    Second Commit: Server Express

--- index.js
+++ index.js
@@ -12,7 +12,7 @@
     // PUT      '/api/productos/:id' -> recibe y actualiza un producto según su id.
     // DELETE   '/api/productos/:id' -> elimina un producto según su id.
 
-// Cada producto estará representado por un objeto con el siguiente formato:
+// Cada producto estará representado por un objeto con el siguiente formato (ver imagen):
 // Cada ítem almacenado dispondrá de un id numérico proporcionado por el backend, comenzando en 1, y que se irá incrementando a medida de que se incorporen productos. Ese id será utilizado para identificar un producto que va a ser listado en forma individual.
 
 // Para el caso de que un producto no exista, se devolverá el objeto: { error : 'producto no encontrado' }
@@ -22,4 +22,75 @@
 // El servidor debe estar basado en express y debe implementar los mensajes de conexión al puerto 8080 y en caso de error, representar la descripción del mismo.
 // Las respuestas del servidor serán en formato JSON. La funcionalidad será probada a través de Postman y del formulario de ingreso.
 
+// npm init --yes
+// npm install modules
+// npm install express
+
+const express = require('express');
+const indexRouter = require('./routers/indexRoutes');
+
+const app = express();
+
+// Configuración STATIC
+app.use(express.json()); 
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname,'public')));
+
+// GET '/api/productos' -> devuelve todos los productos.
+
+app.get('/api/frase', (req, res)=>{
+    res.send(`${frase}`);
+});
+
+// GET '/api/productos/:id' -> devuelve un producto según su id.
+app.get('/api/letras/:num', (req, res)=>{
+    if (isNaN(req.params.num)) {
+        res.send({ error: `El parámetro ${req.params.num} no es un número.`});
+    }else if (req.params.num > frase.length) {
+        res.send({ error: `El parámetro ${req.params.num} es mayor a la cantidad de letras de la frase.`});
+    } else {
+        res.send(frase[parseInt(pos) - 1]);
+    }
+});
+
+// POST '/api/productos' -> recibe y agrega un producto, y lo devuelve con su id asignado.
+app.post('/api/mensajes/', (req, res)=>{
+    console.log('POST request recibido');
+
+    // acá debería crear y guardar un nuevo recurso
+
+    const mensaje = req.body;
+});
+
+// PUT '/api/productos/:id' -> recibe y actualiza un producto según su id.
+app.put('/api/mensajes-json/:id', (req, res)=>{
+    console.log('PUT request recibido');
+
+    // acá debo hallar al recurso con id == req.params.id
+    // y luego reemplazarlo con el registro recibido en req.body
+
+    res.json({
+        result:'ok', 
+        id:req.params.id, 
+        nuevo:req.body
+        });
+});
+
+// DELETE '/api/productos/:id' -> elimina un producto según su id.
+app.delete('/api/mensaje/:id', (req, res)=>{
+    console.log('DELETE request recibido');
+
+    // acá debería eliminar el recurso con id == req.params.id
     
+    res.json({
+        result:'ok', 
+        id:req.params.id, 
+        });
+});
+
+const serverExpress = app.listen(port, () => {
+    console.log(`Server running on port ${serverExpress.address().port}`);
+});
+
+serverExpress.on('error', (error) => {`Error en el servidor: ${error}`});
+
